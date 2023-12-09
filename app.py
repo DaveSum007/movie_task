a366ab48ecd1edeff059e00c2e7b8adb8a9a1c73
Dave Sum <sundawei@ruc.edu.cn>
2023-12-09 15:22:55 +0800

    完成了增加电影条目功能

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 import click
+from datetime import datetime
 
 # 创建flask对象
 app = Flask(__name__)
@@ -54,8 +55,71 @@ def index(page=1):
     movies=pagination.items
     return render_template('index.html',name=admin,movies=movies,pagination=pagination)
 
-@app.route('/add_movie')
+@app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
+    if request.method == 'POST':
+        # 获取表单数据
+        movie_id = request.form['movie_id']
+        movie_name = request.form['movie_name']
+        release_date_str = request.form['release_date']
+        country = request.form['country']
+        movie_type = request.form['type']
+        year = request.form['year']
+        box_office = request.form['box']
+        actor_id = request.form['actor_id']
+        actor_name = request.form['actor_name']
+        gender = request.form['gender']
+        actor_country = request.form['actor_country']
+        relation_type = request.form['relation_type']
+
+        # 转换日期格式
+        release_date = datetime.strptime(release_date_str, '%Y-%m-%d') if release_date_str else None
+
+        # 创建电影信息
+        new_movie = MovieInfo(
+            movie_id=movie_id,
+            movie_name=movie_name,
+            release_date=release_date,
+            country=country,
+            type=movie_type,
+            year=year
+        )
+        db.session.add(new_movie)
+
+        # 创建票房信息
+        if box_office:
+            new_box = MoveBox(
+                movie_id=movie_id,
+                box=float(box_office)
+            )
+            db.session.add(new_box)
+
+        # 创建演员信息，假设每次只添加一位演员
+        new_actor = ActorInfo(
+            actor_id=actor_id,
+            actor_name=actor_name,
+            gender=gender,
+            country=actor_country
+        )
+        db.session.add(new_actor)
+
+        # 创建电影与演员的关系
+        new_relation = MovieActorRelation(
+            movie_id=movie_id,
+            actor_id=actor_id,
+            relation_type=relation_type
+        )
+        db.session.add(new_relation)
+
+        # 提交到数据库
+        db.session.commit()
+
+        # 重定向回主页或确认页面
+        flash('成功添加电影条目！')
+        return redirect(url_for('index'))
+
+    # 如果是 GET 请求，则渲染添加电影条目的页面
+    return render_template('add_movie.html')
 
 @app.errorhandler(404) # 传入要处理的错误代码
 def page_not_found(e): # 接受异常对象作为参数
